326aa446dfdaf7cfe3e8e17a8319d93a196d5410
vkapu <vinodchowdary183@gmail.com>
2022-10-23 17:38:29 -0400

    Used foreach first version

--- Hello-Powershell/conditionaldemo.ps1
+++ Hello-Powershell/conditionaldemo.ps1
@@ -1,7 +1,8 @@
 $servers = @('google.com', 'facebook.com', 'LinkedIn.com', 'Apple.com', 'hgsgxk.com')
 
-foreach ($servername in $servers) {
-    
+
+$servers | foreach-object -Process {($servername in $servers)} {
+    $servername = $_
     $isServerup = Test-Connection -ComputerName $servername -Quiet -Count 1
     
     if ($isServerup -eq $true) {
